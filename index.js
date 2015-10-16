27d13687edecdf9aa44b467e26f841036fd15e43
clarkew <clarke.williams10@gmail.com>
2015-10-15 21:33:16 -0400

    status as of 933, 10-15

---
+++ index.js
@@ -0,0 +1,10 @@
+
+//slideshow of photos take up entire width of the screen it is being viewed on and change every 6 secdonds
+//homepage slideshow
+$(document).ready(function(){
+	console.log("Document Ready");
+	$(".slides").slick({
+		// console.log("test");
+	});
+});
+
